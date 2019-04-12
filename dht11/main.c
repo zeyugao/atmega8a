d0873d870ec6fe10c9db8cb8a5418a6ac6a5a4d6
Elsa Granger <zeyugao@outlook.com>
2019-04-12 19:23:30 +0800

    Create main.c

---
+++ dht11/main.c
@@ -0,0 +1,48 @@
+/*
+ * exp5_dht11_lcd1602.c
+ *
+ * Created: 2019/4/12 18:07:32
+ * Author :
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 1000000L
+#endif
+
+#include <avr/io.h>
+#include "atmega8a_i2c_lcd1602.h"
+#include "dht11.h"
+#include "display.h"
+
+int main(){
+	BYTE toggle = 0;
+	init_display();
+	I2C_LCD1602_Init();
+	BYTE *dht11_lcd1602_data;
+	while (1){
+		DHT11_run();
+		dht11_lcd1602_data = get_dht11_data();
+		I2C_LCD1602_WriteString(0,0,"Temperature :");
+		I2C_LCD1602_WriteNewChar(dht11_lcd1602_data[3] + '0');
+		I2C_LCD1602_WriteNewChar(dht11_lcd1602_data[2] + '0');
+
+		I2C_LCD1602_WriteString(1,0, "Humidity :");
+		I2C_LCD1602_WriteNewChar(dht11_lcd1602_data[1] + '0');
+		I2C_LCD1602_WriteNewChar(dht11_lcd1602_data[0] + '0');
+		
+		display(dht11_lcd1602_data[3]*1000+
+			dht11_lcd1602_data[2]*100+
+			dht11_lcd1602_data[1]*10+
+			dht11_lcd1602_data[0],
+			10);
+		if(!toggle){
+			I2C_LCD1602_WriteChar(1, 15, 0x5c);
+			toggle = 1;
+		}
+		else{
+			I2C_LCD1602_WriteChar(1, 15, 0x20);
+			toggle = 0;
+		}
+		_delay_ms(1100);
+	}
+}
