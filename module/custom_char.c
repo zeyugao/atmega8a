e7468b3e78a47cedfaede7793473bdf51820eb66
Elsa <zeyugao@outlook.com>
2019-04-29 15:27:11 +0800

    update custom_char

--- module/custom_char.c
+++ module/custom_char.c
@@ -4,6 +4,12 @@
 #define OFFSET(p, n) ((1 << (4 - n)) * (p[n]))
 #define PIXEL_MAP(p) (OFFSET(p, 0) | OFFSET(p, 1) | OFFSET(p, 2) | OFFSET(p, 3) | OFFSET(p, 4))
 
-unsigned char pixel_map_to_data(unsigned char map[]) {
+unsigned char pixel_map_to_data(unsigned char map[5]) {
 	return PIXEL_MAP(map);
+}
+
+void gen_bitmap(unsigned char map[8][5], unsigned char output[8]) {
+	for (int i = 0; i < 8; i++) {
+		output[i] = pixel_map_to_data(map[i]);
+	}
 }
--- module/custom_char.h
+++ module/custom_char.h
@@ -1,6 +1,7 @@
 #ifndef CUSTOM_CHAR_H
 #define CUSTOM_CHAR_H
 
-unsigned char pixel_map_to_data(unsigned char map[]);
+unsigned char pixel_map_to_data(unsigned char map[5]);
+void gen_bitmap(unsigned char map[8][5], unsigned char output[8]);
 
 #endif // CUSTOM_CHAR_H
